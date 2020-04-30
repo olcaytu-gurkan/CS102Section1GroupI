5b1635738c0bce687c2938d5b3084e88191742ea
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2020-04-30 12:43:08 +0300

    Fix

--- app/src/main/java/com/example/splashscreenaskit/MainActivity.java
+++ app/src/main/java/com/example/splashscreenaskit/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import com.example.splashscreenaskit.ui.login.LoginActivity;
 
-public class THIRD extends AppCompatActivity
+public class MainActivity extends AppCompatActivity
 {
 
     private static int SPLASH_TIME_OUT = 3000;
