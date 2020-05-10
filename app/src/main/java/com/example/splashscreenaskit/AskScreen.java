3ab2e5d17ede0582603389451f5b0dbe72e1df01
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2020-05-10 07:07:18 +0300

    Fixed AskScreen and QuestionScreen activities

--- app/src/main/java/com/example/splashscreenaskit/AskScreen.java
+++ app/src/main/java/com/example/splashscreenaskit/AskScreen.java
@@ -1,14 +1,8 @@
 package com.example.splashscreenaskit;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.os.Bundle;
 
 public class AskScreen extends AppCompatActivity
 {
@@ -18,19 +12,5 @@ public class AskScreen extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_screen);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
