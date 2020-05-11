2aed3089df74d0737b4e17a49858272cf4b61491
Emirhan Nadir Karaman <nadir.karaman@ug.bilkent.edu.tr>
2020-05-11 19:40:54 +0300

    Merging my part

--- app/src/main/java/com/example/splashscreenaskit/AskingQuestionsScreen.java
+++ app/src/main/java/com/example/splashscreenaskit/AskingQuestionsScreen.java
@@ -1,5 +1,98 @@
 package com.example.splashscreenaskit;
 
-public class AskingQuestionsScreen {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class AskingQuestionsScreen extends AppCompatActivity implements View.OnClickListener {
+
+    // properties
+    EditText editText;
+    TextView addYourTags;
+    EditText tagSpace;
+    ImageButton addButton;
+    TextView tvTags;
+    Button searchButton;
+    String allTags;
+    ArrayList<String> tagsList;
+    String question;
+
+    // constructors
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize
+        editText = (EditText) findViewById(R.id.editText);
+        addYourTags = (TextView) findViewById(R.id.addYourTags);
+        tagSpace = (EditText) findViewById(R.id.tagSpace);
+        addButton = (ImageButton) findViewById(R.id.addTag);
+        tvTags = (TextView) findViewById(R.id.tv_tags);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        allTags = "";
+        tagsList = new ArrayList<String>();
+
+        // listeners
+        addButton.setOnClickListener(this);
+        searchButton.setOnClickListener(this);
+    }
+
+    // methods
+    public void onClick( View v) {
+
+        /** if( v.getId() == addButton.getId()) {
+         *  allTags += tagSpace.getText() + "   ";
+         *  tagsList.add( "" + tagSpace.getText());
+         *  tvTags.setText(allTags)
+         *  }
+         */
+
+        if( v.getId() == addButton.getId()) {
+            for( int i = 0; i < tagsList.size(); i++) {
+                if( tagsList.get(i).equals("" + tagSpace.getText())) {
+                    return;
+                }
+            }
+            allTags += tagSpace.getText() + "   ";
+            tagsList.add("" + tagSpace.getText());
+            tvTags.setText(allTags);
+
+            for( int j = 0; j < tagsList.size(); j++) {
+                System.out.println(tagsList.get(j));
+            }
+        }
+        else if( v.getId() == searchButton.getId()) {
+
+            // GO TO SEARCH RESULTS SCREEN
+            question = "" + editText.getText();
+            // Intent intent;
+            // intent = new Intent(this, OverText.class);
+            // intent.putStringArrayListExtra("tags", tagsList);
+            // HOW DO I GET THE QUESTION???
+            // intent.putExtra("question", question);
+            // startActivity(intent);
+        }
+    }
+
+    //NOTE: IGNORE THE SAME TAG
+
+    /** public void compareTags( Questions ArrayList<Question>) {
+     *    for( int i = 0; i < tagsList.size(); i++) {
+     *        if( question.tagsList.get(i).equals( this.tagsList.get(i)))
+     *    }
+     */
+
+    // public void compareTags( Question question) {
+    //   this.
+    // }
+    // }
 }
---
+++ app/src/main/java/com/example/splashscreenaskit/OverText.java
@@ -0,0 +1,154 @@
+package com.example.splashscreenaskit;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OverText extends AppCompatActivity implements View.OnClickListener {
+    // properties
+    private ImageButton pp;
+    private Profile profile;
+    private int upvote;
+    private int downvote;
+    private int vote;
+    private boolean isUpvoted;
+    private boolean isDownvoted;
+
+    private ImageButton upvoteButton;
+    private ImageButton downvoteButton;
+    private TextView username;
+    private TextView karma;
+
+    private boolean isSelected;
+
+
+    // constructors
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overtext);
+        profile = new Profile();
+        upvote = 0; // question.getUpvoteCount();
+        downvote = 0;
+        vote = upvote - downvote;
+        isUpvoted = false;
+        isDownvoted = false;
+
+        pp = (ImageButton) findViewById(R.id.profileXML);
+        // profile picture
+        if ( profile.getProfilePic() != null )
+            pp = (ImageButton) profile.getProfilePic();
+
+        // username
+       username = (TextView) findViewById(R.id.username);
+       if( profile.getUsername() != null)
+           username.setText( profile.getUsername());
+       else
+           username.setText("anonymous");
+
+        // upvote, downvote buttons
+        upvoteButton = (ImageButton) findViewById(R.id.upvoteButton);
+        downvoteButton = (ImageButton) findViewById(R.id.downvoteButton);
+        karma = (TextView) findViewById( R.id.karma);
+        karma.setText("0");
+
+        // listeners
+        upvoteButton.setOnClickListener(this);
+        downvoteButton.setOnClickListener(this);
+        username.setOnClickListener(this);
+        pp.setOnClickListener(this);
+        karma.setOnClickListener(this);
+
+    }
+
+    // methods
+    @Override
+    public void onClick( View v) {
+        // upvoted
+        if( isUpvoted) {
+            // clicked on upvote button
+            if (v.getId() == upvoteButton.getId()) {
+                upvote--;
+                karma.setText("" + getVote());
+                isUpvoted = !isUpvoted;
+            }
+
+            // clicked on downvote button
+            else if (v.getId() == downvoteButton.getId()) {
+                upvote--;
+                downvote++;
+                karma.setText("" + getVote());
+                isUpvoted = !isUpvoted;
+                isDownvoted = !isDownvoted;
+            }
+        }
+
+        // downvoted
+        else if( isDownvoted){
+            // clicked on upvote button
+            if (v.getId() == upvoteButton.getId()) {
+                downvote--;
+                upvote++;
+                karma.setText("" + getVote());
+                isUpvoted = !isUpvoted;
+                isDownvoted = !isDownvoted;
+            }
+
+            // clicked on downvote button
+            else if (v.getId() == downvoteButton.getId()) {
+                downvote--;
+                karma.setText("" + getVote());
+                isDownvoted = !isDownvoted;
+            }
+        }
+
+        // no actions done yet
+        else if( !isUpvoted && !isDownvoted) {
+            // clicked on upvote button
+            if (v.getId() == upvoteButton.getId()) {
+                upvote++;
+                karma.setText("" + getVote());
+                isUpvoted = !isUpvoted;
+            }
+
+            // clicked on downvote button
+            else if (v.getId() == downvoteButton.getId()) {
+                downvote++;
+                karma.setText("" + getVote());
+                isDownvoted = !isDownvoted;
+            }
+        }
+        // clicked on username or profile picture
+        if( v.getId() == username.getId() || v.getId() == pp.getId()) {
+            // GO TO THE PROFILE SCREEN
+            // Intent intent;
+            // intent = new Intent(this, ProfileScreen.class);
+            // startActivity(intent);
+        }
+
+        // UPVOTE- DOWNVOTE ONCE
+        //  isPressed
+
+    }
+
+    public int getUpvote() { return upvote; }
+
+    public int getDownvote() {
+        return downvote;
+    }
+
+    public int getVote() {
+        return upvote - downvote;
+    }
+
+    public void setVote(int n) {
+        vote = n;
+    }
+
+    public boolean toggle( boolean vote) {
+        return !vote;
+    }
+}
